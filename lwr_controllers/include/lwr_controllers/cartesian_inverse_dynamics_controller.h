dcfb728a189964d08c377fd0c1117fb2af442586
Nicola Piga <nicolapiga@gmail.com>
2017-01-30 12:16:54 +0000

    - change and comment naming conventions for variables - implement evaluation of jacobian, derivative of jacobian and forward kinematics w.r.t a reference point of interest

--- lwr_controllers/include/lwr_controllers/cartesian_inverse_dynamics_controller.h
+++ lwr_controllers/include/lwr_controllers/cartesian_inverse_dynamics_controller.h
@@ -30,27 +30,36 @@ namespace lwr_controllers
     void ForceTorqueCallback(const geometry_msgs::WrenchStamped::ConstPtr& msg);
   
   private:
+
+    // for jacobians x_J_y := Jacobian w.r.t reference point y expressed in basis x
+    // for analytical jacobians x_JA_y := analytical Jacobian w.r.t reference point y expressed in basis x
+    // for rotation matrices R_x_y := Rotation from basis y to basis x
+    // tip := upper part of the 7-th link
+    // ee := reference point of interest
+    // T := euler kinematical matrix
     
     KDL::JntArray C_;
     KDL::JntArrayVel jnt_q_qdot_;
     KDL::JntSpaceInertiaMatrix B_;
-    KDL::Jacobian J_base_, J_w_, J_dot_;
+
+    KDL::Jacobian base_J_tip_, base_J_ee_, ws_J_ee_, ws_J_ee_dot_;
     KDL::Rotation R_w_base_, R_w_ee_;
-    KDL::Frame fk_frame_;
-    KDL::Wrench wrench_wrist_;    
+    KDL::Frame ee_fk_frame_;
+    KDL::Wrench wrench_tip_;    
+    KDL::Chain extended_chain_;
 
     boost::scoped_ptr<KDL::ChainDynParam> dyn_param_solver_;
-    boost::scoped_ptr<KDL::ChainJntToJacSolver> jacobian_solver_;
-    boost::scoped_ptr<KDL::ChainFkSolverPos_recursive> fk_solver_;
-    boost::scoped_ptr<KDL::ChainJntToJacDotSolver> jacobian_dot_solver_;
+    boost::scoped_ptr<KDL::ChainJntToJacSolver> ee_jacobian_solver_, tip_jacobian_solver_;
+    boost::scoped_ptr<KDL::ChainFkSolverPos_recursive> ee_fk_solver_;
+    boost::scoped_ptr<KDL::ChainJntToJacDotSolver> ee_jacobian_dot_solver_;
 
-    Eigen::VectorXd xdot_;
-    Eigen::VectorXd acc_cmd_, tau_fri_;
-    Eigen::Matrix<double, 6,1> bF_;
-    Eigen::Matrix3d T_, T_dot_;
-    Eigen::MatrixXd T_a_, T_a_dot_;;
-    Eigen::MatrixXd wJ_a_, wJ_a_dot_;
-    Eigen::MatrixXd B_inv_, B_a_;
+    Eigen::VectorXd ws_xdot_;
+    Eigen::VectorXd ws_acc_cmd_, tau_fri_;
+    Eigen::Matrix<double, 6,1> base_F_tip_;
+    Eigen::Matrix3d ws_T_, ws_T_dot_;
+    Eigen::MatrixXd ws_TA_, ws_TA_dot_;
+    Eigen::MatrixXd ws_JA_ee_, ws_JA_ee_dot_;
+    Eigen::MatrixXd B_inv_, BA_;
     
     double roll_, pitch_, yaw_;
 
--- lwr_controllers/src/cartesian_inverse_dynamics_controller.cpp
+++ lwr_controllers/src/cartesian_inverse_dynamics_controller.cpp
@@ -18,22 +18,34 @@ namespace lwr_controllers {
   {
     KinematicChainControllerBase<hardware_interface::EffortJointInterface>::init(robot, n);
 
+    // Extend the default chain with a fake segment in order to evaluate
+    // Jacobians, derivatives of jacobians and forward kinematics with respect to given reference point
+    KDL::Joint fake_joint;
+    KDL::Vector offset (0, 0, 0.01); // arm w.r.t the origin of the 7-th link (see urdf)
+    KDL::Frame frame(KDL::Rotation::Identity(), offset);
+    std::string name = "fake_segment";
+    KDL::Segment fake_segment(name, fake_joint, frame);
+    extended_chain_ = kdl_chain_;
+    extended_chain_.addSegment(fake_segment);
+
     dyn_param_solver_.reset(new KDL::ChainDynParam(kdl_chain_, gravity_));
-    jacobian_solver_.reset(new KDL::ChainJntToJacSolver(kdl_chain_));
-    fk_solver_.reset(new KDL::ChainFkSolverPos_recursive(kdl_chain_));
-    jacobian_dot_solver_.reset(new KDL::ChainJntToJacDotSolver(kdl_chain_));
-    jacobian_dot_solver_->setHybridRepresentation();
+    ee_jacobian_solver_.reset(new KDL::ChainJntToJacSolver(extended_chain_));
+    tip_jacobian_solver_.reset(new KDL::ChainJntToJacSolver(kdl_chain_));
+    ee_fk_solver_.reset(new KDL::ChainFkSolverPos_recursive(extended_chain_));
+    ee_jacobian_dot_solver_.reset(new KDL::ChainJntToJacDotSolver(extended_chain_));
+    ee_jacobian_dot_solver_->setHybridRepresentation();
     
     B_.resize(kdl_chain_.getNrOfJoints());
     C_.resize(kdl_chain_.getNrOfJoints());
-    J_base_.resize(kdl_chain_.getNrOfJoints());
-    J_w_.resize(kdl_chain_.getNrOfJoints());
-    J_dot_.resize(kdl_chain_.getNrOfJoints());
+    base_J_ee_.resize(kdl_chain_.getNrOfJoints());
+    base_J_tip_.resize(kdl_chain_.getNrOfJoints());
+    ws_J_ee_.resize(kdl_chain_.getNrOfJoints());
+    ws_J_ee_dot_.resize(kdl_chain_.getNrOfJoints());
 
-    wrench_wrist_ = KDL::Wrench();
+    wrench_tip_ = KDL::Wrench();
 
-    xdot_ = Eigen::VectorXd(6);
-    acc_cmd_ = Eigen::VectorXd(6);
+    ws_xdot_ = Eigen::VectorXd(6);
+    ws_acc_cmd_ = Eigen::VectorXd(6);
     tau_fri_ = Eigen::VectorXd(kdl_chain_.getNrOfJoints());
 
     sub_ = n.subscribe("/lwr/ft_sensor", 1, &CartesianInverseDynamicsController::ForceTorqueCallback, this);
@@ -49,11 +61,11 @@ namespace lwr_controllers {
     }
 
     for(size_t i=0; i<6; i++)
-      acc_cmd_(i) = 0;
+      ws_acc_cmd_(i) = 0;
 
     R_w_base_ = KDL::Rotation::RotY(-M_PI / 2);
-    T_a_ = MatrixXd::Zero(6,6);
-    T_a_dot_ = MatrixXd::Zero(6,6);
+    ws_TA_ = MatrixXd::Zero(6,6);
+    ws_TA_dot_ = MatrixXd::Zero(6,6);
   }
 
   void CartesianInverseDynamicsController::update(const ros::Time& time, const ros::Duration& period)
@@ -65,54 +77,55 @@ namespace lwr_controllers {
     }
 
     // solvers
-    // evaluate the current B(q)
+    // evaluate the current B(q) (B is evaluated without taking into account anything past the wrist)
     dyn_param_solver_->JntToMass(joint_msr_states_.q, B_);
-    // evaluate thecurrent C(q) * q_dot
+    // evaluate thecurrent C(q) * q_dot (C is evaluated without taking into account anything past the wrist)
     dyn_param_solver_->JntToCoriolis(joint_msr_states_.q, joint_msr_states_.qdot, C_);
-    // evaluate the current geometric jacobian J(q)
-    jacobian_solver_->JntToJac(joint_msr_states_.q, J_base_);
-    // evaluate the current homogeneous transformation from the base to the last link fk_frame
-    fk_solver_->JntToCart(joint_msr_states_.q, fk_frame_);
+    // evaluate the current geometric jacobian base_J_ee(q)
+    ee_jacobian_solver_->JntToJac(joint_msr_states_.q, base_J_ee_);
+    tip_jacobian_solver_->JntToJac(joint_msr_states_.q, base_J_tip_);
+    // evaluate the current homogeneous transformation from the base to the point of interest ee_fk_frame
+    ee_fk_solver_->JntToCart(joint_msr_states_.q, ee_fk_frame_);
     
     /////////////////////////////////////////////////////////////////////////
     //
-    // evaluate the analytical jacobian written w.r.t. the intermediate frame wJ_a
-    // wJ_a = T_a * wJ
+    // evaluate the analytical jacobian written w.r.t. the intermediate frame ws_JA_ee
+    // ws_JA_ee = ws_TA * wJ
     //
     /////////////////////////////////////////////////////////////////////////
     
-    // get the current RPY attitude representation PHI from R_w_base * fk_frame.M
-    R_w_ee_ = R_w_base_ * fk_frame_.M;
+    // get the current RPY attitude representation PHI from R_w_base * ee_fk_frame.M
+    R_w_ee_ = R_w_base_ * ee_fk_frame_.M;
     R_w_ee_.GetEulerZYX(yaw_, pitch_, roll_);
     
-    // evaluate the transformation matrix between the geometric and analytical jacobian T_a
-    // T_a = [eye(3), zeros(3);
+    // evaluate the transformation matrix between the geometric and analytical jacobian TA
+    // ws_TA = [eye(3), zeros(3);
     //        zeros(3), inv(T(PHI))]
     // where T is the Euler Kinematical Matrix
-    eul_kin_RPY(pitch_, yaw_, T_);
-    T_a_.block<3,3>(0,0) = Matrix<double, 3, 3>::Identity();
-    T_a_.block<3,3>(3,3) = T_.inverse();
+    eul_kin_RPY(pitch_, yaw_, ws_T_);
+    ws_TA_.block<3,3>(0,0) = Matrix<double, 3, 3>::Identity();
+    ws_TA_.block<3,3>(3,3) = ws_T_.inverse();
 
     // evaluate wJ
-    KDL::changeBase(J_base_, R_w_base_, J_w_);
+    KDL::changeBase(base_J_ee_, R_w_base_, ws_J_ee_);
 
-    wJ_a_ = T_a_ * J_w_.data;
+    ws_JA_ee_ = ws_TA_ * ws_J_ee_.data;
     //
     ////////////////////////////////////////////////////////////////////////
 
 
     ////////////////////////////////////////////////////////////////////////
     //
-    // evaluate the kinetic pseudo-energy B_a (Siciliano p. 297)
+    // evaluate the kinetic pseudo-energy BA (Siciliano p. 297)
     //
     ////////////////////////////////////////////////////////////////////////
 
     // evaluate the inverse of the joint space inertia matrix B
     B_inv_ = B_.data.inverse();
 
-    // B_a = inv(wJ_a * B_inv * J_base_')
-    B_a_ = wJ_a_ * B_inv_ * J_base_.data.transpose();
-    B_a_ = B_a_.inverse();
+    // BA = inv(ws_JA_ee * B_inv * base_J_ee_')
+    BA_ = ws_JA_ee_ * B_inv_ * base_J_ee_.data.transpose();
+    BA_ = BA_.inverse();
     //
     ////////////////////////////////////////////////////////////////////////
 
@@ -120,36 +133,36 @@ namespace lwr_controllers {
     ////////////////////////////////////////////////////////////////////////
     //
     // evaluate part of the Coriolis compensation in operational space
-    // B_a * dot(wJ_a) * qdot
+    // BA * dot(ws_JA_ee) * qdot
     //
     ////////////////////////////////////////////////////////////////////////
     //
     
-    // evaluation of dot(wJ_a) = d/dt{T_a} * J_w_ + T_a * d/dt{J_w_}
+    // evaluation of dot(ws_JA_ee) = d/dt{ws_TA} * ws_J_ee_ + ws_TA * d/dt{ws_J_ee_}
     //
-    // where d/dt{T_a} = [d/dt{eye(3)}, d/dt{zeros(3)}; 
+    // where d/dt{ws_TA} = [d/dt{eye(3)}, d/dt{zeros(3)}; 
     //                   d/dt{zeros(3)}, d/dt{inv(T(PHI))}]
     //                 = [zeros(3), zeros(3);
     //                    zeros(3), -inv(T) * d/dt{T} * int(T)]
     //
-    // and d/dt{J_w_} = [R_w_base_, zeros(3);
-    //                      zeros(3), R_w_base_] * d/dt{J_base_}
+    // and d/dt{ws_J_ee_} = [R_w_base_, zeros(3);
+    //                      zeros(3), R_w_base_] * d/dt{base_J_ee_}
     
     // evaluate the derivative of the state using the analytical jacobian
-    xdot_ = wJ_a_ * joint_msr_states_.qdot.data;
+    ws_xdot_ = ws_JA_ee_ * joint_msr_states_.qdot.data;
 
-    eul_kin_RPY_dot(pitch_, yaw_, xdot_(4), xdot_(3), T_dot_);
-    T_a_dot_.block<3,3>(3,3) = - T_.inverse() * T_dot_ * T_.inverse();
+    eul_kin_RPY_dot(pitch_, yaw_, ws_xdot_(4), ws_xdot_(3), ws_T_dot_);
+    ws_TA_dot_.block<3,3>(3,3) = - ws_T_.inverse() * ws_T_dot_ * ws_T_.inverse();
 
-    // evaluate the derivative of the jacobian J_base_
+    // evaluate the derivative of the jacobian base_J_ee_
     jnt_q_qdot_.q = joint_msr_states_.q;
     jnt_q_qdot_.qdot = joint_msr_states_.qdot;
-    jacobian_dot_solver_->JntToJacDot(jnt_q_qdot_, J_dot_);
+    ee_jacobian_dot_solver_->JntToJacDot(jnt_q_qdot_, ws_J_ee_dot_);
     
     // and project it in the intermediate frame
-    J_dot_.changeBase(R_w_base_);
+    ws_J_ee_dot_.changeBase(R_w_base_);
 
-    wJ_a_dot_ = T_a_dot_ * J_w_.data + T_a_ * J_dot_.data;
+    ws_JA_ee_dot_ = ws_TA_dot_ * ws_J_ee_.data + ws_TA_ * ws_J_ee_dot_.data;
     //
     ////////////////////////////////////////////////////////////////////////
 
@@ -157,21 +170,21 @@ namespace lwr_controllers {
     ////////////////////////////////////////////////////////////////////////
     //
     // evaluate force and torque compensation
-    // bF = 
+    // base_F_tip = 
     //     [R_base_wrist, zeros(3);
     //      zeros(3), R_base_wrist_] * wF
     //
     ////////////////////////////////////////////////////////////////////////
     //
 
-    wrench_wrist_ = fk_frame_.M * wrench_wrist_;
-    tf::wrenchKDLToEigen(wrench_wrist_, bF_);      
+    wrench_tip_ = ee_fk_frame_.M * wrench_tip_;
+    tf::wrenchKDLToEigen(wrench_tip_, base_F_tip_);      
     
     //
     ////////////////////////////////////////////////////////////////////////
 
     // evaluate tau_cmd
-    tau_fri_ = C_.data + J_base_.data.transpose()* B_a_ * (bF_ + acc_cmd_ - wJ_a_dot_ * joint_msr_states_.qdot.data);
+    tau_fri_ = C_.data + base_J_tip_.data.transpose()* BA_ * (base_F_tip_ + ws_acc_cmd_ - ws_JA_ee_dot_ * joint_msr_states_.qdot.data);
 
     // set tau
     for(size_t i = 0; i < joint_handles_.size(); i++) 
@@ -180,7 +193,7 @@ namespace lwr_controllers {
 
   void CartesianInverseDynamicsController::ForceTorqueCallback(const geometry_msgs::WrenchStamped::ConstPtr& msg)
   {
-    tf::wrenchMsgToKDL(msg->wrench, wrench_wrist_);
+    tf::wrenchMsgToKDL(msg->wrench, wrench_tip_);
   }
     
 } // namespace
