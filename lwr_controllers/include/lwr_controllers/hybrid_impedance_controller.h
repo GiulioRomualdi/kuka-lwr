6e64944acd562d8c5bed0b27dd4c84bcb308be8c
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-02-01 18:23:40 +0100

    implement set_cmd service

--- lwr_controllers/include/lwr_controllers/hybrid_impedance_controller.h
+++ lwr_controllers/include/lwr_controllers/hybrid_impedance_controller.h
@@ -3,6 +3,9 @@
 
 #include "cartesian_inverse_dynamics_controller.h"
 
+// service
+#include "lwr_controllers/HybridSetCmd.h"
+
 namespace lwr_controllers
 {
   class HybridImpedanceController: public CartesianInverseDynamicsController
@@ -13,13 +16,19 @@ namespace lwr_controllers
     ~HybridImpedanceController();
     
     bool init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n);
+
+    bool set_cmd(lwr_controllers::HybridSetCmd::Request &req,\
+		 lwr_controllers::HybridSetCmd::Response &res);
     
     void starting(const ros::Time& time);
 
     void update(const ros::Time& time, const ros::Duration& period);
 
   private:
-    
+
+    // service
+    ros::ServiceServer set_cmd_service_;
+
     KDL::Wrench ws_F_ee_;
     KDL::Rotation R_des_;
 
@@ -28,8 +37,8 @@ namespace lwr_controllers
     Eigen::VectorXd err_x_;
     Eigen::MatrixXd Kp_, Kd_;    
 
-    double time_;
-
+    //double time_;
+    double fz_des_;
   };
 } // namespace
 
--- lwr_controllers/src/hybrid_impedance_controller.cpp
+++ lwr_controllers/src/hybrid_impedance_controller.cpp
@@ -23,13 +23,17 @@ namespace lwr_controllers {
       xdot_des_ = Eigen::VectorXd(6);
       xdotdot_des_ = Eigen::VectorXd(6);
       err_x_ = Eigen::VectorXd(6);
+    
+      // service
+      set_cmd_service_ = n.advertiseService("set_cmd", &HybridImpedanceController::set_cmd, this); 
+
       return true;
   }
 
   void HybridImpedanceController::starting(const ros::Time& time)
   {
     CartesianInverseDynamicsController::starting(time);
-    time_ = 0;
+    //time_ = 0;
     
     // controller gains
     Kp_ = Eigen::Matrix<double, 6, 6>::Identity() * 20;
@@ -37,6 +41,8 @@ namespace lwr_controllers {
 
     for(int i = 0; i < 6; i++)
       acc_cmd_(i) = 0;
+
+    fz_des_ = 0;
   }
 
   void HybridImpedanceController::update(const ros::Time& time, const ros::Duration& period)
@@ -54,27 +60,27 @@ namespace lwr_controllers {
     KDL::Frame force_transformation(R_ws_base_, R_ws_ee_ * (-p_wrist_ee_)); 
     ws_F_ee_ = force_transformation * base_wrench_wrist_;
 
-    time_ = time_ + period.toSec();
-    double f = 0.1;
-    double omega = 2 * M_PI * f;
-    double rho = 0.1;
-    double x_trj = rho * cos(omega * time_);
-    double y_trj = rho * sin(omega * time_);
-    double z_trj = 0.02;
-    double dx_trj = -omega * rho * sin(omega * time_);
-    double dy_trj = omega * rho * cos(omega * time_);
-    double dz_trj = 0;
-    double ddx_trj = -omega * omega * rho * cos(omega * time_);
-    double ddy_trj = -omega * omega * rho * sin(omega * time_);
-    double ddz_trj = 0;
-    double yaw_des, pitch_des, roll_des;
+    // time_ = time_ + period.toSec();
+    // double f = 0.1;
+    // double omega = 2 * M_PI * f;
+    // double rho = 0.1;
+    // double x_trj = rho * cos(omega * time_);
+    // double y_trj = rho * sin(omega * time_);
+    // double z_trj = 0.02;
+    // double dx_trj = -omega * rho * sin(omega * time_);
+    // double dy_trj = omega * rho * cos(omega * time_);
+    // double dz_trj = 0;
+    // double ddx_trj = -omega * omega * rho * cos(omega * time_);
+    // double ddy_trj = -omega * omega * rho * sin(omega * time_);
+    // double ddz_trj = 0;
+    // double yaw_des, pitch_des, roll_des;
 
     // define the desired state
-    R_des_ = KDL::Rotation::RotY(0) * KDL::Rotation::RotY(M_PI);
-    R_des_.GetEulerZYX(yaw_des, pitch_des, roll_des);
-    x_des_ << x_trj, y_trj, z_trj, yaw_des, pitch_des, roll_des;
-    xdot_des_ << dx_trj, dy_trj, dz_trj, 0, 0, 0;
-    xdotdot_des_ << ddx_trj, ddy_trj, ddz_trj, 0, 0, 0;
+    // R_des_ = KDL::Rotation::RotY(0) * KDL::Rotation::RotY(M_PI);
+    // R_des_.GetEulerZYX(yaw_des, pitch_des, roll_des);
+    // x_des_ << x_trj, y_trj, z_trj, yaw_des, pitch_des, roll_des;
+    // xdot_des_ << dx_trj, dy_trj, dz_trj, 0, 0, 0;
+    // xdotdot_des_ << ddx_trj, ddy_trj, ddz_trj, 0, 0, 0;
     
     err_x_ = x_des_ - ws_x_;
     err_x_(3) = angles::normalize_angle(err_x_(3));
@@ -87,14 +93,29 @@ namespace lwr_controllers {
     // acc_cmd_(3) = acc_cmd_(3) - ws_F_ee_.torque.x();
     // acc_cmd_(4) = acc_cmd_(4) - ws_F_ee_.torque.y();
     // acc_cmd_(5) = acc_cmd_(5) - ws_F_ee_.torque.z();
-    acc_cmd_(2) = 0.1 * (-10 * ws_xdot_(2) + (-0.4 - ws_F_ee_.force.z()));
+    acc_cmd_(2) = 0.1 * (-10 * ws_xdot_(2) + (fz_des_ - ws_F_ee_.force.z()));
 
     set_command(acc_cmd_);
 
     //std::cout << ((-1) - ws_F_ee_.force.z()) << std::endl;
     // std::cout << ws_F_ee_.force.z() << "\t"  <<  wrench_wrist_.force.z() <<  "\t" << base_wrench_wrist_.force.x() << std::endl;
   }
-    
+
+  bool HybridImpedanceController::set_cmd(lwr_controllers::HybridSetCmd::Request &req,\
+					  lwr_controllers::HybridSetCmd::Response &res)
+  {
+    double yaw_des, pitch_des, roll_des;
+    R_des_ = KDL::Rotation::RotY(0) * KDL::Rotation::RotY(M_PI);
+    R_des_.GetEulerZYX(yaw_des, pitch_des, roll_des);
+    x_des_ << req.command[0], req.command[1], 0, yaw_des, pitch_des, roll_des;
+    xdot_des_ << 0, 0, 0, 0, 0, 0;
+    xdotdot_des_ << 0, 0, 0, 0, 0, 0;
+    fz_des_ = req.command[2];
+
+    res.setted = true;
+    return true;
+  }
+
 } // namespace
 
 PLUGINLIB_EXPORT_CLASS(lwr_controllers::HybridImpedanceController, controller_interface::ControllerBase)
