1f751673203beabb17d7c54fb6103ed7c59f3ece
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-02-03 12:02:33 +0100

    implement end-effector compensation in cartesian position controller

--- lwr_controllers/include/lwr_controllers/cartesian_position_controller.h
+++ lwr_controllers/include/lwr_controllers/cartesian_position_controller.h
@@ -3,13 +3,13 @@
 
 #include "KinematicChainControllerBase.h"
 
-//#include <visualization_msgs/Marker.h>
-#include <std_msgs/Float64MultiArray.h>
+// msg include
+#include <geometry_msgs/WrenchStamped.h>
 
 #include <boost/scoped_ptr.hpp>
 
 /*
-  tau_cmd_ = B(q) * y + C * dq
+  tau_cmd_ = B(q) * y + C * dq + J' * F
   y =  - Kp * q - Kd * dq + r
   r = Kp * q_d
   q_d = inverse_kinematics(x_d)
@@ -26,15 +26,11 @@ namespace lwr_controllers
     ~CartesianPositionController();
      		
     bool init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n);
-
     void starting(const ros::Time& time);
-
     void update(const ros::Time& time, const ros::Duration& period);
-    void set_qd(const std_msgs::Float64MultiArray::ConstPtr &msg);
+    void force_torque_callback(const geometry_msgs::WrenchStamped::ConstPtr& msg);
 
   private:
-    
-    ros::Subscriber sub_posture_;
 
     // joint position controller
     KDL::JntArray q_msr_, q_des_, dotq_msr_;
@@ -48,13 +44,21 @@ namespace lwr_controllers
 
     // inverse kinematics
     boost::scoped_ptr<KDL::ChainIkSolverPos_LMA> ik_solver_;
-    boost::scoped_ptr<KDL::ChainFkSolverPos_recursive> fk_solver_;
     boost::scoped_ptr<KDL::ChainIkSolverVel_wdls> ik_vel_solver_;
     KDL::Frame x_des_;
     KDL::Rotation des_attitude_;
     KDL::Vector des_pose_;
 
-    
+    // force compensation
+    // WARNING: the end effector is compensated using force-torque sensor only
+    //          instead of using B, C and G matrix
+    ros::Subscriber sub_force_;
+    KDL::Frame ee_fk_frame_;
+    KDL::Wrench wrench_wrist_, base_wrench_wrist_;
+    Eigen::Matrix<double, 6,1> base_F_wrist_;
+    boost::scoped_ptr<KDL::ChainFkSolverPos_recursive> fk_solver_;
+    boost::scoped_ptr<KDL::ChainJntToJacSolver> jacobian_solver_;
+    KDL::Jacobian J_;
   };
 
 } // namespace
--- lwr_controllers/src/cartesian_position_controller.cpp
+++ lwr_controllers/src/cartesian_position_controller.cpp
@@ -10,6 +10,9 @@
 #include <angles/angles.h>
 #include <lwr_controllers/cartesian_position_controller.h>
 
+#include <kdl_conversions/kdl_msg.h>
+#include <eigen_conversions/eigen_kdl.h>
+
 namespace lwr_controllers {
 
   CartesianPositionController::CartesianPositionController() {}
@@ -21,13 +24,15 @@ namespace lwr_controllers {
     KinematicChainControllerBase<hardware_interface::EffortJointInterface>::init(robot, n);
 
     dyn_param_solver_.reset(new KDL::ChainDynParam(kdl_chain_, gravity_));
-    // fk_solver_.reset(new KDL::ChainFkSolverPos_recursive(kdl_chain_));
+    fk_solver_.reset(new KDL::ChainFkSolverPos_recursive(kdl_chain_));
     // ik_vel_solver_.reset(new KDL::ChainIkSolverVel_wdls(kdl_chain_));
-    //    ik_solver_.reset(new KDL::ChainIkSolverPos_NR_JL(kdl_chain_, joint_limits_.min, joint_limits_.max, *fk_solver_, *ik_vel_solver_));
+    // ik_solver_.reset(new KDL::ChainIkSolverPos_NR_JL(kdl_chain_, joint_limits_.min, joint_limits_.max, *fk_solver_, *ik_vel_solver_));
     ik_solver_.reset(new KDL::ChainIkSolverPos_LMA(kdl_chain_));
+    jacobian_solver_.reset(new KDL::ChainJntToJacSolver(kdl_chain_));
 
     K_.resize(kdl_chain_.getNrOfJoints());
     D_.resize(kdl_chain_.getNrOfJoints());
+    J_.resize(kdl_chain_.getNrOfJoints());
 
     ROS_DEBUG(" Number of joints in handle = %lu", joint_handles_.size() );
   
@@ -39,10 +44,10 @@ namespace lwr_controllers {
     C_.resize(kdl_chain_.getNrOfJoints());
     M_tau_cmd_.resize(kdl_chain_.getNrOfJoints());    
 
-    sub_posture_ = nh_.subscribe("set_qd", 1, &CartesianPositionController::set_qd, this);
-
     des_pose_ = KDL::Vector::Zero();
     des_attitude_ = KDL::Rotation::Identity();
+
+    sub_force_ = n.subscribe("/lwr/ft_sensor", 1, &CartesianPositionController::force_torque_callback, this);
     
     return true;
   }
@@ -86,28 +91,23 @@ namespace lwr_controllers {
     dyn_param_solver_->JntToMass(q_msr_, M_);
     dyn_param_solver_->JntToCoriolis(q_msr_, dotq_msr_, C_);
 
+    // compute end-effector compensation
+    fk_solver_->JntToCart(q_msr_, ee_fk_frame_);
+    jacobian_solver_->JntToJac(q_msr_, J_);
+    base_wrench_wrist_ = ee_fk_frame_.M * wrench_wrist_;
+    tf::wrenchKDLToEigen(base_wrench_wrist_, base_F_wrist_);      
+
     // evaluate M * tau_cmd
-    M_tau_cmd_.data = M_.data * tau_cmd_.data + C_.data;
+    M_tau_cmd_.data = M_.data * tau_cmd_.data + C_.data - J_.data.transpose() * base_F_wrist_;
 
     // set tau
     for(size_t i=0; i<joint_handles_.size(); i++) 
       joint_handles_[i].setCommand(M_tau_cmd_(i));
   }
 
-
-  void CartesianPositionController::set_qd(const std_msgs::Float64MultiArray::ConstPtr &msg){
-    if (msg->data.size() == 0) {
-      ROS_INFO("Desired configuration must be: %lu dimension", joint_handles_.size());
-    }
-    else if ((int)msg->data.size() != joint_handles_.size()) {
-      ROS_ERROR("Posture message had the wrong size: %d", (int)msg->data.size());
-      return;
-    }
-    else
-      {
-	for (unsigned int j = 0; j < joint_handles_.size(); ++j)
-	  q_des_(j) = msg->data[j];
-      }
+  void CartesianPositionController::force_torque_callback(const geometry_msgs::WrenchStamped::ConstPtr& msg)
+  {    
+    tf::wrenchMsgToKDL(msg->wrench, wrench_wrist_);
   }
 
 }// namespace
